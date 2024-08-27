b4e0c02492ee60fd1f2ea010c0ac6903e9ce5366
Florian <mrminecrafttv19@gmail.com>
2024-08-27 02:24:10 +0200

    chore: commentary

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import routes from "virtual:generated-pages-react";
-import {useRoutes} from "react-router";
-import {BrowserRouter} from "react-router-dom";
+import { useRoutes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
-import './globals.css'
+import './globals.css';
 
-
+/**
+ * The main application component.
+ * Uses `useRoutes` to render the routes defined in the `routes` object.
+ *
+ * @returns {JSX.Element} The rendered routes.
+ */
 const App = () => {
     return useRoutes(routes);
 };
 
+// Create a root element and render the application within it.
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
+);
