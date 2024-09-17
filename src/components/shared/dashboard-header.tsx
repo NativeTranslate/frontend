e508c8a6d2817249cff06304e230c23457dc9839
Florian <mrminecrafttv19@gmail.com>
2024-09-17 17:46:50 +0200

    feat: stuff

--- src/components/shared/dashboard-header.tsx
+++ src/components/shared/dashboard-header.tsx
@@ -1,18 +1,25 @@
-import UserButton from '@/components/shared/user-button.tsx';
+import React from 'react';
+import UserButton from '@/components/shared/user-button';
 
 interface Props {
     title: string;
+    children?: React.ReactNode;
 }
 
-const DashboardHeader = ({ title }: Props) => {
+const DashboardHeader: React.FC<Props> = ({ title, children }) => {
     return (
-        <div className="flex flex-row items-center bg-dark-300 rounded-3xl p-4">
-            <p className="text-2xl text-white-900 font-semibold">{title}</p>
-            <div className="flex-grow" />
-            <div className={'flex gap-5'}>
+        <div className="flex flex-row items-center justify-between bg-dark-300 rounded-3xl p-4">
+            <div className="flex items-center">
+                <h1 className="text-2xl text-white-900 font-semibold">
+                    {title}
+                </h1>
+            </div>
+            <div className="flex items-center gap-5">
+                {children}
                 <UserButton />
             </div>
         </div>
     );
 };
+
 export default DashboardHeader;
